9b67c51692cfa88292a24d9c404a698ba1b004a3
gapry <gapry@protonmail.com>
2025-03-02 22:26:32 +0800

    Add MIT License and contributors tracking (#12)

---
+++ Contributors.md
@@ -0,0 +1,7 @@
+# Contributors
+
+This list is sorted alphabetically by name.
+
+- [Cecilia](https://github.com/ceciliachan1979)
+- [Gapry](https://github.com/gapry)
+- (To be updated as contributors join)
--- ekans/parser.rkt
+++ ekans/parser.rkt
@@ -1,3 +1,6 @@
+; Copyright (c) 2025 Good Night, Good Morning and contributors (see Contributors.md)
+; Licensed under the MIT License. See the LICENSE file in the project root for details.
+
 #lang racket
 
 (provide parse-statement)
