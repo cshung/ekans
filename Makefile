e294885d34729edb28d478d8a30838e74d264ede
gapry <gapry@protonmail.com>
2025-03-14 00:11:45 +0800

    runtime: environment: init (#37)

--- Makefile
+++ Makefile
@@ -1,6 +1,10 @@
 # Copyright (c) 2025 Good Night, Good Morning and contributors (see Contributors.md)
 # Licensed under the MIT License. See the LICENSE file in the project root for details.
 
+INCLUDES = -I./inc
+CFLAGS   = -fsanitize=address -Wall -Werror -g
+LDFLAGS  = -lasan
+
 default: clean build execute
 
 fmt:
@@ -25,23 +29,27 @@ execute:
 clean:
 	rm -rf *.out *.c build/
 
+test-all: clean unit-tests test-compiled-c-code test-runtime
+
 test-compiled-c-code: build
-	set -e;                                                              \
-	for file in $$(find test/data -name '*.rkt' | sed 's/\.rkt$$//'); do \
-		if [ -f "$$file.expect" ]; then                                    \
-			rm -f ./build/output.actual;                                     \
-			./build/compiler.out "$$file.rkt";                               \
-			clang -g -I inc -o build/app.out build/main.c build/ekans.o;     \
-			./build/app.out > ./build/output.actual;                         \
-			diff "$$file.expect" ./build/output.actual;                      \
-			diff -q "$$file.expect" ./build/output.actual >/dev/null;        \
-		fi;                                                                \
+	set -e;                                                                      \
+	for file in $$(find test/data -name '*.rkt' | sed 's/\.rkt$$//'); do         \
+		if [ -f "$$file.expect" ]; then                                            \
+			rm -f ./build/output.actual;                                             \
+			./build/compiler.out "$$file.rkt";                                       \
+			clang $(INCLUDES) $(CFLAGS) -o build/app.out build/main.c build/ekans.o; \
+			./build/app.out > ./build/output.actual;                                 \
+			diff "$$file.expect" ./build/output.actual;                              \
+			diff -q "$$file.expect" ./build/output.actual >/dev/null;                \
+		fi;                                                                        \
 	done
 
 test-runtime: clean fmt
 	mkdir -p build
-	clang -g -I ./inc -c -o build/ekans.o runtime/ekans.c
-	clang -g -I ./inc -o build/test-runtime.out test/runtime/main.c build/ekans.o 
+	clang $(INCLUDES) $(CFLAGS) -c -o build/ekans.o runtime/ekans.c
+	clang $(INCLUDES) $(CFLAGS) -c -o build/environment.o runtime/environment.c
+	clang $(INCLUDES) $(CFLAGS) -o build/test-runtime.out test/runtime/main.c build/ekans.o build/environment.o
+	# ASAN_OPTIONS=detect_leaks=1 ./build/test-runtime.out # OSX doesn't work
 	./build/test-runtime.out
 
 unit-tests: fmt
--- test/runtime/main.c
+++ test/runtime/main.c
@@ -2,6 +2,7 @@
 #include <assert.h>
 #include <ekans-internals.h>
 #include <ekans.h>
+#include <environment.h>
 
 void test_initialize_ekans() {
   initialize_ekans();
@@ -41,11 +42,111 @@ void test_create_boolean_value() {
   printf("[%s] passed\n", __FUNCTION__);
 }
 
+void test_environment(void) {
+  ekans_environment* global_environment = create_environment(NULL, 1);
+  if (global_environment == NULL) {
+    printf("Error: Failed to create global environment\n");
+    return;
+  }
+
+  ekans_closure* plus_closure = malloc(sizeof(ekans_closure));
+  if (plus_closure == NULL) {
+    printf("Error: Failed to create ekans closure\n");
+    return;
+  }
+  plus_closure->closure           = global_environment;
+  plus_closure->function          = plus;
+  global_environment->bindings[0] = (ekans_value*)plus_closure;
+
+  {
+    int p = 1, q = 2;
+
+    ekans_environment* local_environment = create_environment(global_environment, 2);
+    set_environment(local_environment, 0, create_number_value(p));
+    set_environment(local_environment, 1, create_number_value(q));
+
+    ekans_value* plus_function = get_environment(global_environment, 0, 0);
+    if (plus_function == NULL) {
+      printf("Error: Failed to get plus_function\n");
+      return;
+    }
+
+    ekans_value* result = NULL;
+    int          status = plus_closure->function(local_environment, &result);
+    if (status == 0) {
+      printf("%-4d + %-4d = ", p, q);
+      print_ekans_value(result);
+    } else {
+      printf("Error: Failed to execute function\n");
+    }
+
+    free(local_environment->bindings);
+    free(local_environment);
+  }
+
+  {
+    int p = 12, q = 23;
+
+    ekans_environment* local_environment = create_environment(global_environment, 2);
+    set_environment(local_environment, 0, create_number_value(p));
+    set_environment(local_environment, 1, create_number_value(q));
+
+    ekans_value* plus_function = get_environment(global_environment, 0, 0);
+    if (plus_function == NULL) {
+      printf("Error: Failed to get plus_function\n");
+      return;
+    }
+
+    ekans_value* result = NULL;
+    int          status = plus_closure->function(local_environment, &result);
+    if (status == 0) {
+      printf("%-4d + %-4d = ", p, q);
+      print_ekans_value(result);
+    } else {
+      printf("Error: Failed to execute function\n");
+    }
+
+    free(local_environment->bindings);
+    free(local_environment);
+  }
+
+  {
+    int p = -123, q = -321;
+
+    ekans_environment* local_environment = create_environment(global_environment, 2);
+    set_environment(local_environment, 0, create_number_value(p));
+    set_environment(local_environment, 1, create_number_value(q));
+
+    ekans_value* plus_function = get_environment(global_environment, 0, 0);
+    if (plus_function == NULL) {
+      printf("Error: Failed to get plus_function\n");
+      return;
+    }
+
+    ekans_value* result = NULL;
+    int          status = plus_closure->function(local_environment, &result);
+    if (status == 0) {
+      printf("%-4d + %-4d = ", p, q);
+      print_ekans_value(result);
+    } else {
+      printf("Error: Failed to execute function\n");
+    }
+
+    free(local_environment->bindings);
+    free(local_environment);
+  }
+
+  free(plus_closure);
+  free(global_environment->bindings);
+  free(global_environment);
+}
+
 int main() {
   printf("=====================\n");
   test_initialize_ekans();
   test_create_number_value();
   test_create_boolean_value();
+  test_environment();
   printf("=====================\n");
   printf("All tests passed!\n");
   return 0;
